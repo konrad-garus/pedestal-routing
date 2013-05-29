1fd81b62f5b0f7d46aeb13a6b2720f03b327597c
konrad-garus <konrad.garus@gmail.com>
2013-05-29 10:19:20 +0200

    Better routing, heavy use of Pedestal not necessary

--- README.md
+++ README.md
@@ -2,9 +2,14 @@ Demo of client-side routing with Pedestal using goog.History.
 
 There are two implementations of it available under different git tags: 
  * `simple-hardcoded` - very easy to grasp, but quite inelegant and not 
- reusable.
+ reusable. Makes heavy use of Pedestal - following a route means pushing a 
+ message to the input queue and DOM reaction doesn't happen until a renderer.
  * `generic` - not much more complicated, shows one way to generalize that
- solution. Your mileage may vary, there is more than one way to do it.
+ solution. Your mileage may vary, there is more than one way to do it. It also
+ uses Pedestal heavily.
+ * `generic-light` - lightweight alternative to `generic`. It does not need
+ Pedestal for routing at all, but it enables you to interact with Pedestal if
+ that's what you want.
 
 To launch the application, run the following:
 
--- app/src/routing_app/app.cljs
+++ app/src/routing_app/app.cljs
@@ -1,12 +1,10 @@
 ; This is a demo of client-side routing with Google Closure and Pedestal.
 ;
-; It sets up a listener with goog.History which pushes the location token into
-; the app queue. That message is passed through default pipeline up to the app
-; model. Finally, there is a renderer which listens to such changes in the app
-; model and actually performs a DOM manipulation.
+; It sets up a listener with goog.History. As soon as the location changes, it
+; determines the right action based the configuration provided and executes it.
 ;
-; This demo shows one generic way to do it - renderer is a reusable HOF 
-; configured with a special structure. See below for details. 
+; This demo is limited to the rendering layer, but the handlers can push 
+; messages to the input queue if desired.
 
 (ns routing-app.app
   (:require [io.pedestal.app :as app]
@@ -20,42 +18,32 @@
             [goog.History :as history]
             [goog.events :as goog-event]))
 
-; Apparently both of those things are necessary. All they do is push the :route
-; message up to the app model
-(def count-app {:transform {:route {:init nil :fn #(:value %2)}}
-                :emit {:router {:fn app/default-emitter-fn :input #{:route}}}})
-
-(defn ^:private set-route [input-queue route]
-  (p/put-message input-queue {msg/topic :route msg/type :set-route :value route}))
-
-; Bridge to goog.History from Closure. All it really does is take the path token
-; and push it in a :route message to the input queue.
-(defn configure-router 
-  ([input-queue] (configure-router input-queue ""))
-  ([input-queue default-route]
-    (doto (goog.History.)
-      (goog.events/listen (goog.object/getValues goog.history/EventType) 
-                          (fn [e]
-                            (let [token (.-token e)]
-                              (if (= "" token)
-                                (set-route input-queue default-route)
-                                (set-route input-queue token)))))
-    (.setEnabled true))))
-
-; Generic route renderer. cfg is a map with the following options:
+; Generic view-only route renderer. Config is a map with the following options:
 ;
 ; :routes {"a" function-a "b" function-b} - Call function-a for path a, 
 ; function-b for path b.
 ; 
 ; :listener (fn [old-value new-value] ...) - Whenever route changes, call this
 ; function.
-(defn route-renderer [cfg]
-  (fn [_ [_ _ old-value new-value] input-queue]
-    (when-let [listener (:listener cfg)]
-      (listener old-value new-value))
-    (if-let [dispatcher (get-in cfg [:routes new-value])]
-      (dispatcher)
-      (.log js/console "Unknown route:" new-value))))
+;
+; :default-route "my-route" - When the route is empty, use "my-route".
+(defn ^:private route-changed [{:keys [input]} route-config]
+  (fn [e]
+    (let [token (.-token e)
+          token (if (= "" token) (:default-route route-config) token)]
+      (when-let [listener (:listener cfg)]
+        (listener token))
+      (if-let [dispatcher (get-in route-config [:routes token])]
+        (dispatcher input)
+        (.log js/console "Unknown route:" token)))))
+
+(defn configure-router 
+  ([app route-config]
+    (doto (goog.History.)
+      (goog.events/listen 
+        (goog.object/getValues goog.history/EventType) 
+        (route-changed app route-config))
+      (.setEnabled true))))
 
 ; Specific renderers for this demo application, nothing interesting or generic here. 
 (defn render-route [msg]
@@ -63,10 +51,13 @@
     (dom/destroy-children! container)
     (dom/append! container (str "<p>" msg "</p>"))))
 
-(defn route-first []
+; These renderers only touch DOM, but they can do something completely different 
+; as well. They could push a message to the input queue without doing any DOM
+; manipulation, or do both.
+(defn route-first [input-queue]
   (render-route "This is the first route"))
 
-(defn route-second []
+(defn route-second [input-queue]
   (render-route "This is the second route"))
 
 ; Some "real", working config.
@@ -74,17 +65,14 @@
   {:routes {"first" route-first
             "second" route-second}
    :default-route "first"
-   :listener (fn [old-value new-value]
-               (.log js/console "Routing from" old-value "to" new-value))})
+   :listener (fn [new-value]
+               (.log js/console "Routing to" new-value))})
 
 ; The rendering is at least a two-phase process and it needs to be configured in
 ; two places - the goog.History listener to push messages to input queue, and 
 ; the renderer which updates DOM as those changes occur. 
 (defn ^:export main []
-  (let [app (app/build count-app)
-        ; Plug in renderer here...
-        render-fn (push/renderer "content" [[:value [:route] (route-renderer router-config)]])]
+  (let [app (app/build count-app)]
     (render/consume-app-model app render-fn)
-    ; ... and configure the history listener here
-    (configure-router (:input app) (:default-route router-config))
+    (configure-router app router-config)
     (app/begin app)))
